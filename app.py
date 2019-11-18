6a4fbb500ac7936656176f9d257ca25453933f4e
Vladislav Maraev <vlad@maraev.me>
2019-11-18 18:22:08 +0300

    Change to debug, and way of running

--- app.py
+++ app.py
@@ -18,3 +18,6 @@ def hello():
     sentence = request.args.get("sentence", "Россия объявила в международный розыск журналиста Александра Шварева из-за публикаций о миллиардере Алишере Усманове.")
     parse = stanford_parse(sentence) 
     return render_template('parse.html',sentence=sentence,parse=parse)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='8090')
